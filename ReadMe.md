443d56f67916cf93dece3b3adbee27dc75488d73
Frostfire <frostfire.onex@gmail.com>
2025-09-23 05:17:16 +0600

    new init

--- ReadMe.md
+++
@@ -1,7 +0,0 @@
-# ONEX Emotes List
-
-This script serves as an emote provider, allowing you to add any custom animations you own or use available open-source emotes. You can easily add new emotes to the `AnimList.lua` file.
-
-By default, the script includes the **rpemotes** emote list and streams as a example.
-
-The emote streams used in this project are sourced from **rpemotes-reborn**. The original script can be found at the [rpemotes-reborn GitHub repository](https://github.com/alberttheprince/rpemotes-reborn/tree/master).
--- fxmanifest.lua
+++ fxmanifest.lua
@@ -14,28 +14,8 @@ fx_version 'cerulean'
 game 'gta5'
 version '0.0.1'
 this_is_a_map 'yes'
--- ====================  Resource UI  ========================================================
-files {'conditionalanims.meta'}
 -- ==================== Resource Configuration ===============================================
 shared_scripts {'locales.lua', 'client/client.lua', 'client/locales/*.lua', 'AnimList.lua'}
 dependencies {'/server:7290', '/onesync'}
-
-data_file 'DLC_ITYP_REQUEST' 'stream/rpemotesreborn_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/brummie_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_camp_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/apple_1.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/kaykaymods_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/knjgh_pizzas.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/natty_props_lollipops.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/ultra_ringcase.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/pata_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/vedere_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/pnwsigns.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/pprp_icefishing.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/scully_props.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/samnick_prop_lighter01.ytyp'
-data_file 'DLC_ITYP_REQUEST' 'stream/bzzz_murderpack.ytyp'
-
 -- ==================== Resource Code Configuration  =============================================
 lua54 'yes'
