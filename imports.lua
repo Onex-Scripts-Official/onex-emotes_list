f29bc7da236cf3e94fc4b51a657ca1ae057e1594
Frostfire <frostfire.onex@gmail.com>
2025-09-25 05:03:58 +0600

    Refactor emote loading logic for improved clarity and structure

--- imports.lua
+++ imports.lua
@@ -1,46 +1,44 @@
--- local Emotes = {expressions, scenarios, walk_styles, general, dance_emotes, animal, consumable, exits, prop_emotes,
---                 shared_emotes, all_emotes, shared_dances}
+local load = false
+local Emotes = {
+    expressions = expressions,
+    scenarios = scenarios,
+    walk_styles = walk_styles,
+    general = general,
+    dance_emotes = dance_emotes,
+    animal = animal,
+    consumable = consumable,
+    exits = exits,
+    prop_emotes = prop_emotes,
+    shared_emotes = shared_emotes,
+    all_emotes = all_emotes,
+    shared_dances = shared_dances
+}
+
 -- Load all emote configuration files
--- local function LoadEmotesData()
---     -- All Emotes 
---     emotesData.all_emotes = {}
---     -- Combine all emotes into a single table
---     for category, data in pairs(emotesData) do
---         if type(data) == "table" then
---             if data.options then
---                 -- For emote categories with options structure (general, dance, etc.)
---                 for _, emote in pairs(data.options) do
---                     emote.originalCategory = category
---                     table.insert(emotesData.all_emotes, emote)
---                 end
---             elseif category ~= "all_emotes" and category ~= "exits" then
---                 for _, item in pairs(data) do
---                     item.originalCategory = category
---                     table.insert(emotesData.all_emotes, item)
---                 end
---             end
---         end
---     end
---     -- shared dance init 
---     emotesData.shared_dances = emotesData.dance_emotes
---     return true
--- end
+local function LoadEmotesData()
+    -- All Emotes 
+    Emotes.all_emotes = {}
+
+    -- shared dance init
+    Emotes.shared_dances = Emotes.dance_emotes
+    load = true
+    return true
+end
+
 -- -- Initialize data on resource start
--- CreateThread(function()
---     LoadEmotesData()
--- end)
--- Export functions for other scripts to use
+CreateThread(function()
+    LoadEmotesData()
+end)
+
 -- Get all emotes data
+
 exports('RetrieveEmotes', function()
+    repeat
+        Wait(50)
+    until load
     return Emotes
 end)
 
--- exports('GetScenarioModels', function()
-
---     local scenario_models = LoadResourceFile(GetCurrentResourceName(), 'config/scenario_models.lua')
---     if scenario_models then
---         return load(scenario_models)()
---     end
-
---     return false
--- end)
+exports('GetScenarioModels', function()
+    return scenario_models
+end)
