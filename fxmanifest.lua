827cd765e3803636041801058cbcebd4b47c52b5
Frostfire <frostfire.onex@gmail.com>
2025-09-30 16:34:51 +0600

    Fix formatting inconsistencies in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -12,12 +12,12 @@ PowerdBy 'Onex Scripts'
 -- ====================== Game Configuration =================================================
 fx_version 'cerulean'
 game 'gta5'
-version '2.0.0'
+version '0.0.1'
 this_is_a_map 'yes'
 -- ==================== Resource Configuration ===============================================
 -- New modular config system with backward compatibility
-shared_scripts {'config/*.lua', 'config/emotes/*.lua'}
+shared_scripts { 'config/*.lua', 'config/emotes/*.lua' }
 client_script 'imports.lua'
-dependencies {'/server:7290', '/onesync'}
+dependencies { '/server:7290', '/onesync' }
 -- ==================== Resource Code Configuration  =============================================
 lua54 'yes'
